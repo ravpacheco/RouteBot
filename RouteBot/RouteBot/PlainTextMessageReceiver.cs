66e182ff7bc54ec673aa5373ec3ce1daab48517f
Rafael Avelar Pacheco <rafaelpa@takenet.com.br>
2016-03-23 17:31:42 -0300

    Init message receiver

--- RouteBot/RouteBot/PlainTextMessageReceiver.cs
+++ RouteBot/RouteBot/PlainTextMessageReceiver.cs
@@ -1,5 +1,10 @@
 using Lime.Protocol;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Takenet.MessagingHub.Client;
 using Takenet.MessagingHub.Client.Receivers;
@@ -13,6 +18,50 @@ namespace RouteBot
             Console.WriteLine($"From: {message.From} \tContent: {message.Content}");
             await EnvelopeSender.SendMessageAsync("Pong!", message.From);
             await EnvelopeSender.SendNotificationAsync(message.ToConsumedNotification());
+        }
+
+        private async Task ExecuteSearchAsync(Message message, string uri)
+        {
+            using (var request = new HttpRequestMessage(HttpMethod.Get, uri))
+            {
+                using (var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5)))
+                {
+                    var client = new WebClient();
+                    var buscapeResponse = await client.DownloadStringAsync(new Uri(uri));
+                    if (buscapeResponse.StatusCode != HttpStatusCode.OK)
+                    {
+                        await EnvelopeSender.SendMessageAsync(@"Não foi possível obter uma resposta do Buscapé!", message.From);
+                    }
+                    else
+                    {
+                        var resultJson = await buscapeResponse.Content.ReadAsStringAsync();
+                        dynamic responseMessage = JsonConvert.DeserializeObject(resultJson);
+                        try
+                        {
+                            foreach (JObject product in responseMessage.product)
+                            {
+                                try
+                                {
+                                    //var resultItem = ParseProduct(product);
+                                    //await EnvelopeSender.SendMessageAsync(resultItem, message.From);
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine($"Exception parsing product: {e}");
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Exception parsing response from Buscapé: {e}");
+                            await EnvelopeSender.SendMessageAsync("Nenhum resultado encontrado", message.From);
+                            return;
+                        }
+                        await Task.Delay(TimeSpan.FromSeconds(2), cancellationTokenSource.Token);
+                        await EnvelopeSender.SendMessageAsync($"Envie: ; ", message.From);
+                    }
+                }
+            }
         }
     }
 }
